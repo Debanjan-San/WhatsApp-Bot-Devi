d82dee29145d2db807b330e48a1ad985487537aa
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2023-10-27 00:15:12 +0500

    chore: WELL

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-import { useMultiFileAuthState, fetchLatestBaileysVersion } from '@whiskeysockets/baileys'
 import P from 'pino'
 import Devi from './libs/Devi.js'
-;(async () => {
-    const { state, saveCreds } = await useMultiFileAuthState('session')
-    const config = {
-        name: 'devi',
-        mods: [],
-        prefix: '.'
-    }
-
-    new Devi(config, saveCreds, {
-        version: (await fetchLatestBaileysVersion()).version,
-        auth: state,
-        logger: P({ level: 'silent' }),
-        printQRInTerminal: true
-    }).connect()
-})()
+;(async () => await new Devi().connect())()
