610bc0d48a069847e4987bb86227555be7ee7820
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2023-11-22 21:33:32 +0500

    chore: update

--- src/handler/Message.js
+++ src/handler/Message.js
@@ -1,29 +1,37 @@
 import { join } from 'path'
-import { URL } from 'url'
-export default class MessageHandler {
-    commands = new Map()
-    aliases = new Map()
+const __dirname = path.resolve()
 
+export default class MessageHandler {
+    /**
+     * @param {client} client
+     */
     constructor(client) {
+        /**
+         * @type {Client}
+         */
         this.client = client
     }
 
-    handler = async (M) => {
-        const context = this.parseArgs(M.content)
-        const { args, cmd } = context
-        if (!args[0] || !args[0].startsWith(this.client.config.prefix))
-            return void this.client.log.notice(`(MSG): from ${M.pushName} in ${M.group?.title || 'Direct Message'}`)
-        const isCommand = M.content.startsWith(this.client.config.prefix)
-        if (!isCommand) return
-        const command = this.commands.get(cmd) || this.aliases.get(cmd)
+    /**
+     * @param {Message} M
+     * @returns {Promise<void>}
+     */
 
-        this.client.log(`(CMD): ${args[0]}[${args.length - 1}])} from ${sender.username} in ${M.chat}`)
+    handler = async (M) => {
+        const { prefix } = this.client.config
+        const args = M.content.split(' ')
+        const title = M.chat === 'group' ? M.group?.title || 'Group' : 'DM'
+        if (!args[0] || !args[0].startsWith(prefix))
+            return void this.client.log.notice(`Message from ${M.sender.username} in ${title}`)
+        this.client.log(`(CMD): ${args[0]}[${args.length - 1}])} from ${M.sender.username} in ${title}`)
+        const cmd = args[0].toLowerCase().slice(prefix.length)
+        const command = this.commands.get(cmd) || this.aliases.get(cmd)
         if (!command) return void M.reply('No Command Found! Try using one from the help list.')
-        if (!command.config?.dm && M.chat === 'dm') return void M.reply('This command can only be used in groups')
+        if (M.chat === 'dm' && !command.config?.dm) return void M.reply('This command can only be used in groups')
         if (M.chat === 'group' && command.config?.adminOnly && !M.isAdminMessage)
             return void M.reply(`Only admins are allowed to use this command`)
         try {
-            await command.exec(M, context)
+            await command.exec(M, this.parseArgs(args))
         } catch (err) {
             return void this.client.log.error(err.message)
         }
@@ -31,13 +39,12 @@ export default class MessageHandler {
 
     loadCommands = () => {
         this.client.log.info('Loading Commands...')
-        const __dirname = new URL('.', import.meta.url).pathname
         const path = join(__dirname, '..', 'commands')
         const files = this.client.util.readdirRecursive(path)
         files.map((file) => {
             const filename = file.split('/')
             if (!filename[filename.length - 1].startsWith('_')) {
-                const command = new (require(file).default)(this.client, this)
+                const command = new (await import(path)).default(this.client, this)
                 this.commands.set(command.config.command, command)
                 if (command.config.aliases) command.config.aliases.forEach((alias) => this.aliases.set(alias, command))
                 this.client.log.info(`Loaded: ${command.config.command} from ${file}`)
@@ -47,20 +54,32 @@ export default class MessageHandler {
         this.client.log.notice(`Successfully Loaded ${this.commands.size} Commands`)
     }
 
-    parseArgs = (raw) => {
-        const args = raw.split(' ')
-        const cmd = args.shift()?.toLocaleLowerCase().slice(this.client.config.prefix.length) ?? ''
-        const text = args.join(' ')
-        const flags = {}
-        for (const arg of args) {
-            if (arg.startsWith('--')) {
-                const [key, value] = arg.slice(2).split('=')
-                flags[key] = value
-            } else if (arg.startsWith('-')) {
-                flags[arg] = ''
-            }
+    /**
+     * @type {Map<string, Command>}
+     */
+
+    commands = new Map()
+
+    /**
+     * @type {Map<string, Command>}
+     */
+
+    aliases = new Map()
+
+    /**
+     * @private
+     * @param {string[]} args
+     * @returns {args}
+     */
+
+    parseArgs = (args) => {
+        const cmd = args[0].toLowerCase().slice(this.client.config.prefix.length).trim()
+        args.splice(0, 1)
+        return {
+            cmd,
+            args,
+            text: args.join(' ').trim(),
+            flags: args.filter((arg) => arg.startsWith('--'))
         }
-        // prettier-ignore
-        return { cmd, text, flags, args, raw }
     }
 }
