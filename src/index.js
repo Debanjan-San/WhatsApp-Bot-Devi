29dc6f2f0e8faa25178d20f491683eb1beb6fdcd
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2023-12-01 07:43:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ import Devi from './libs/Devi.js'
 ;(async () => {
     const log = createLogger()
     const config = getConfig()
-    if (!config.url) {
+    if (!config.mongo) {
         log.error('No mongo url provided')
         return process.exit(1)
     }
-    const mongo = new MongoClient(config.url, {
+    const mongo = new MongoClient(config.mongo, {
         socketTimeoutMS: 1_00_000,
         connectTimeoutMS: 1_00_000,
         waitQueueTimeoutMS: 1_00_000
     })
     const databaseHandler = new DatabaseHandler(config, log)
-    const collection = mongo.db('session').collection('auth')
+    const collection = mongo.db(config.session).collection('auth')
     const authSession = await useMongoDBAuthState(collection)
     new Devi(config, mongo, authSession, log, databaseHandler, {
         version: (await fetchLatestBaileysVersion()).version,
