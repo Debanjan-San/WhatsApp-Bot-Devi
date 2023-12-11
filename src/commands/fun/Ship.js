bbf3a3e50003d6e2c70e52b6cc35390ea5e64256
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2023-12-11 12:59:33 +0530

    Update Ship.js

--- src/commands/fun/Ship.js
+++ src/commands/fun/Ship.js
@@ -1,5 +1,5 @@
 import BaseCommand from '../../libs/BaseCommand.js'
-import { Ship } from '@shineiichijo/canvas-chan'
+import canvafy from 'canvafy'
 
 export default class Command extends BaseCommand {
     constructor(client, handler) {
@@ -15,38 +15,51 @@ export default class Command extends BaseCommand {
     }
 
     exec = async (M) => {
-        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        while (M.mentioned.length < 2) M.mentioned.unshift(M.sender.jid)
-        if (M.mentioned.includes(this.client.user.id)) return void (await M.reply("❌ You can't ship the bot"))
-        const users = await Promise.all(
-            M.mentioned.slice(0, 2).map(async (id) => {
-                const { username } = await this.client.DB.getUserInfo(id, this.client)
-                return {
-                    name: username,
-                    image: await this.client.util.fetchBuffer(
-                        (await this.client.profilePictureUrl(id, 'image')) ??
-                            'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
-                    )
-                }
-            })
-        )
-        const level = this.level()
-        let text = ''
-        if (level >= 0 && level < 10) text = 'Awful'
-        else if (level >= 10 && level < 25) text = 'Very Bad'
-        else if (level >= 25 && level < 40) text = 'Poor'
-        else if (level >= 40 && level < 55) text = 'Average'
-        else if (level >= 55 && level < 75) text = 'Good'
-        else if (level >= 75 && level < 90) text = 'Great'
-        else if (level >= 90) text = 'Amazing'
+        try {
+            if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+            while (M.mentioned.length < 2) M.mentioned.unshift(M.sender.jid)
+            if (M.mentioned.includes(this.client.user.id)) return void (await M.reply("❌ You can't ship the bot"))
+            const images = await Promise.all(
+                M.mentioned.slice(0, 2).map(async (id) => {
+                    return {
+                        jid: id,
+                        image: await this.client
+                            .profilePictureUrl(id, 'image')
+                            .catch(
+                                () =>
+                                    'https://static.wikia.nocookie.net/v__/images/7/73/Fuseu404notfound.png/revision/latest?cb=20171104190424&path-prefix=vocaloidlyrics'
+                            )
+                    }
+                })
+            )
+            const level = this.level()
+            let text = ''
+            if (level >= 0 && level < 10) text = 'Awful'
+            else if (level >= 10 && level < 25) text = 'Very Bad'
+            else if (level >= 25 && level < 40) text = 'Poor'
+            else if (level >= 40 && level < 55) text = 'Average'
+            else if (level >= 55 && level < 75) text = 'Good'
+            else if (level >= 75 && level < 90) text = 'Great'
+            else if (level >= 90) text = 'Amazing'
 
-        const image = await new Ship(users, level, text).build()
-        return void (await M.replyRaw({
-            image,
-            caption: `\`\`\`🔺Compatibility Meter🔺\`\`\`
-💖 *${users[0].name} x ${users[1].name}* 💖
+            const url = images.map((i) => i.image)
+            const image = await new canvafy.Ship()
+                .setAvatars(url[0], url[1])
+                .setBackground('image', 'https://i.pinimg.com/originals/8b/22/7e/8b227ee6d17a97b4f867b506bbd99c81.jpg')
+                .setBorder('#FF0000')
+                .setCustomNumber(level)
+                .setOverlayOpacity(0.5)
+                .build()
+            return void (await M.replyRaw({
+                image,
+                mentions: images.map((i) => i.jid),
+                caption: `\`\`\`🔺Compatibility Meter🔺\`\`\`
+💖 *${images.map((i) => `@${i.jid.split('@')[0]}`).join(' X ')}* 💖
 *🔻 ${level} ${level < 50 ? '💔' : '💞'} ${text}* 🔻`
-        }))
+            }))
+        } catch (e) {
+            console.error(e)
+        }
     }
 
     level = () => Math.floor(Math.random() * 100) + 1
