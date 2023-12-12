bdb4e01b7de320a5cc90f43c1bcae184fb91651f
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2023-12-13 00:52:45 +0530

    update

---
+++ src/commands/utils/Telesticker.js
@@ -0,0 +1,38 @@
+import BaseCommand from '../../libs/BaseCommand.js'
+import { Sticker, StickerTypes } from 'wa-sticker-formatter'
+
+export default class Command extends BaseCommand {
+    constructor(client, handler) {
+        super(client, handler, {
+            command: 'telesticker',
+            category: 'utils',
+            aliases: ['tl'],
+            description: {
+                content: 'Convert telegram stickers into whatsapp Stickers',
+                usage: '[link]'
+            },
+            dm: true,
+            exp: 8
+        })
+    }
+
+    exec = async (M) => {
+        if (!M.urls.length) return void (await M.reply('❌ Link?'))
+        const url = M.urls.find((url) => url.includes('t.me/addstickers'))
+        if (!url) return void (await M.reply('❌ No telegram addstickers URLs found in your message'))
+        const { name, title, stickers } = await this.client.util.fetch(
+            `https://weeb-api.vercel.app/telesticker?url=${url}`
+        )
+        await M.reply('✔️ Sending strickers in your private chat')
+        for (const sticker of stickers) {
+            await this.client.sendMessage(M.sender.jid, {
+                sticker: await new Sticker(sticker, {
+                    pack: name,
+                    author: title,
+                    type: StickerTypes.FULL,
+                    quality: 70
+                }).build()
+            })
+        }
+    }
+}
