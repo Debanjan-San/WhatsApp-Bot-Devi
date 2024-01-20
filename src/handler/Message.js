9e7f5bd9780a9ff6e1b38ad0e64d8c2b129e50ec
Amrit9304 <91752236+Amrit9304@users.noreply.github.com>
2024-01-20 19:07:03 +0530

    Update Message.js

--- src/handler/Message.js
+++ src/handler/Message.js
@@ -140,19 +140,23 @@ export default class MessageHandler {
 
     loadCommands = async () => {
         this.client.log.info('Loading Commands...')
-        const __dirname = new URL('.', import.meta.url).pathname
-        const path = join(__dirname, '..', 'commands')
-        const files = this.client.util.readdirRecursive(path)
+        
+        const currentWorkingDir = process.cwd();
+        const path = join(currentWorkingDir, 'src', 'commands');
+        const files = this.client.util.readdirRecursive(path);
         for (const file of files) {
-            const filename = file.split('/')
+            const filename = file.split('/');
             if (!filename[filename.length - 1].startsWith('_')) {
-                const command = new (Object.values(await import(file))[0])(this.client, this)
-                this.commands.set(command.config.command, command)
-                if (command.config.aliases) command.config.aliases.forEach((alias) => this.aliases.set(alias, command))
-                this.client.log.info(`Loaded: ${command.config.command} from ${command.config.category}`)
+                const filePath = 'file://' + file.replace(/\\/g, '/');
+                const command = new (Object.values(await import(filePath))[0])(this.client, this);
+                this.commands.set(command.config.command, command);
+                if (command.config.aliases) {
+                    command.config.aliases.forEach((alias) => this.aliases.set(alias, command));
+                }
+                this.client.log.info(`Loaded: ${command.config.command} from ${command.config.category}`);
             }
         }
-        this.client.log.notice(`Successfully Loaded ${this.commands.size} Commands`)
+        this.client.log.notice(`Successfully Loaded ${this.commands.size} Commands`);
     }
 
     parseArgs = (raw) => {
