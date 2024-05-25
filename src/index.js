6579010f3d8356ac37374e99a1ff02d52f4c5913
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2024-05-25 23:00:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import P from 'pino'
-import { fetchLatestBaileysVersion } from '@whiskeysockets/baileys'
 import Devi from './libs/Devi.js'
 import { createLogger } from './utils/Logger.js'
 import getConfig from './getConfig.js'
@@ -17,8 +16,9 @@ import DatabaseHandler from './handler/Database.js'
     const { useDatabaseAuth } = new AuthenticationFromDatabase(config.session, database)
     const authSession = await useDatabaseAuth()
     new Devi(config, authSession, log, database, {
-        version: (await fetchLatestBaileysVersion()).version,
+        version:  [2, 2413, 1],
         auth: authSession.state,
+        syncFullHistory: false,
         logger: P({ level: 'fatal' }),
         printQRInTerminal: true,
         getMessage: async () => {
